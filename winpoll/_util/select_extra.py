dfd7e18c70ad4c1902af7ccc6a347df537ec7717
James Edington Administrator <codegeek98@gmail.com>
2025-07-25 16:30:16 -0500

    attribution comments

--- winpoll/_util/select_extra.py
+++ winpoll/_util/select_extra.py
@@ -14,42 +14,42 @@ __all__ = [
 ]
 
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L325
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L314
 try: POLLERR = select.POLLERR
 except AttributeError: POLLERR = 0x0001
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L326
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L315
 try: POLLHUP = select.POLLHUP
 except AttributeError: POLLHUP = 0x0002
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L327
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L316
 try: POLLNVAL = select.POLLNVAL
 except AttributeError: POLLNVAL = 0x0004
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L328
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L317
 try: POLLWRNORM = select.POLLWRNORM
 except AttributeError: POLLWRNORM = 0x0010
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L329
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L318
 try: POLLWRBAND = select.POLLWRBAND
 except AttributeError: POLLWRBAND = 0x0020
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L330
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L319
 try: POLLRDNORM = select.POLLRDNORM
 except AttributeError: POLLRDNORM = 0x0100
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L331
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L320
 try: POLLRDBAND = select.POLLRDBAND
 except AttributeError: POLLRDBAND = 0x0200
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L332
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L321
 try: POLLPRI = select.POLLPRI
 except AttributeError: POLLPRI = 0x0400
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L333
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L322
 try: POLLIN = select.POLLIN
 except AttributeError: POLLIN = (POLLRDNORM | POLLRDBAND)
 
-# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L334
+# https://github.com/wine-mirror/wine/blob/wine-10.12/include/winsock2.h#L323
 try: POLLOUT = select.POLLOUT
 except AttributeError: POLLOUT = POLLWRNORM
